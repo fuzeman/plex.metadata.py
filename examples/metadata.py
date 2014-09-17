425386a454ca397b4cc3c4a86984737abcfa2577
Dean Gardiner <gardiner91@gmail.com>
2014-09-17 16:24:50 +1200

    Replaced old metadata cache with a generic mapping interface (shove)

--- examples/metadata.py
+++ examples/metadata.py
@@ -2,8 +2,9 @@ import logging
 logging.basicConfig(level=logging.DEBUG)
 
 from plex import Plex
-from plex.ext.activity import Activity
-from plex.ext.metadata import Guid, Matcher, Metadata
+from plex_activity import Activity
+from plex_metadata import Guid, Matcher, Metadata
+from shove import Shove
 
 def get_item(key):
     episode = Metadata.get(3)
@@ -17,6 +18,8 @@ def get_item(key):
 
 
 if __name__ == '__main__':
+    Metadata.cache = Shove('memory://', 'memory://')
+
     get_item(3)
 
     Activity.start()
